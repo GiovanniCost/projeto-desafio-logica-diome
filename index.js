6d24ea1b0e983f859bf52ea55eb3bc3122e76712
GiovanniCost <gcsilvab@gmail.com>
2026-05-22 19:33:56 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let heroName = "";
+let xp = 0;
+
+switch (true) {
+  case xp <= 1000:
+    console.log(`O Herói de nome ${heroName} está no nível Ferro!`);
+    break;
+  case xp >= 1001 && xp <= 2000:
+    console.log(`O Herói de nome ${heroName} está no nível Bronze!`);
+    break;
+  case xp >= 2001 && xp <= 5000:
+    console.log(`O Herói de nome ${heroName} está no nível Prata!`);
+    break;
+  case xp >= 6001 && xp <= 7000:
+    console.log(`O Herói de nome ${heroName} está no nível Ouro!`);
+    break;
+  case xp >= 7001 && xp <= 8000:
+    console.log(`O Herói de nome ${heroName} está no nível Platina!`);
+    break;
+  case xp >= 8001 && xp <= 9000:
+    console.log(`O Herói de nome ${heroName} está no nível Ascendente!`);
+    break;
+  case xp >= 9001 && xp <= 10000:
+    console.log(`O Herói de nome ${heroName} está no nível Imortal!`);
+    break;
+  case xp >= 10001:
+    console.log(`O Herói de nome ${heroName} está no nível Radiante!`);
+    break;
+
+  default:
+    console.log(`O Herói de nome ${heroName} não tem classificação de nivel!`);
+    break;
+}
